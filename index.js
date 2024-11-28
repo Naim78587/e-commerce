e4b5d8ffc1bd90c2117359996a35921ab97a4c42
Naim78587 <naimahmed1921@gmail.com>
2024-11-29 05:40:30 +0600

    responsive image slider

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ const menuIcon = document.getElementById('manu-icon');
             menu.classList.toggle('hidden');
             menu.classList.toggle('active');
         });
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const sliderImages = document.querySelector('.slider__images');
+            const slides = document.querySelectorAll('.slider__images img');
+            const totalSlides = slides.length;
+            let currentIndex = 0;
+    
+            const autoSlide = () => {
+                currentIndex = (currentIndex + 1) % totalSlides; 
+                sliderImages.scrollTo({
+                    left: sliderImages.offsetWidth * currentIndex,
+                    behavior: 'smooth'
+                });
+            };
+    
+            setInterval(autoSlide, 3000);
+        });
